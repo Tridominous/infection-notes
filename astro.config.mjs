0f269fb8034cae53bbd1e4e49c05ae63833a85f4
Tridominous <jamesmou02@gmail.com>
2026-03-05 20:49:39 +0000

    Changed case

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://Tridominous.github.io',
+  site: 'https://tridominous.github.io',
   base: '/infection-notes',
   integrations: [tailwind()],
   markdown: {
